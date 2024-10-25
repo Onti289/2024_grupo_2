a25cd5e1637ba472c40b4ec1f04a856d281f9d6c
LautaCarron <166958031+LautaCarron@users.noreply.github.com>
2024-10-25 15:48:55 -0300

    AUTO CAPA MODELOIDE

--- TPFinal_TdPI/Client/src/main/java/test/TestAuto.java
+++ TPFinal_TdPI/Client/src/main/java/test/TestAuto.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import modeloDatos.*;
+
 public class TestAuto {
 
 	@Before
@@ -17,8 +19,40 @@ public class TestAuto {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void testConstructorAuto() {
+        // Valores de prueba
+        String patente = "ABC";
+        int cantidadPlazas = 2;
+        boolean mascota = false;
+
+        // Crear instancia del auto
+        Auto auto = new Auto(patente, cantidadPlazas, mascota);
+
+        // Verificar que los atributos se asignaron correctamente
+        assertEquals("La patente debería ser ABC", "ABC", auto.getPatente());
+        assertEquals("La cantidad de plazas debería ser 2", 2, auto.getCantidadPlazas());
+        assertFalse("No debería permitir mascota", auto.isMascota());
+    }
+	
+	@Test
+    public void testgetPuntajePedido() {
+        Auto auto1 = new Auto("AAA", 4, true);
+        Auto auto2 = new Auto("AAA", 4, false);
+        
+        Cliente cliente = new Cliente("lau","atr","lautaro");
+        
+        Pedido pedido1 = new Pedido(cliente, 2, true, true, 10, "ZONA_STANDARD");
+        Pedido pedido2 = new Pedido(cliente, 8, false, false, 10, "ZONA_STANDARD");
+        
+        int prueba1 = auto1.getPuntajePedido(pedido1);
+        assertEquals("El puntaje debería ser 80", 80, prueba1);
+        Integer prueba2 = auto1.getPuntajePedido(pedido2);
+        assertNull("El objeto 2 deberia ser null", prueba2);
+        Integer prueba3 = auto2.getPuntajePedido(pedido1);
+        assertNull("El objeto 3 deberia ser null", prueba3);
+     
+    }
+	
+	
 
 }
---
+++ TPFinal_TdPI/Client/src/main/java/test/TestChofer.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestChofer {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+    
+    
+}
