ea784e702433d92f9f547863e2f514822ea35779
AgusC02 <agustin02cerda@gmail.com>
2024-10-31 20:21:19 -0300

    Capa de negocios actualizada

--- test/testModelo_de_negocios/TestEmpresa.java
+++ test/testModelo_de_negocios/TestEmpresa.java
@@ -18,9 +18,11 @@ import excepciones.VehiculoRepetidoException;
 import modeloDatos.Auto;
 import modeloDatos.Chofer;
 import modeloDatos.ChoferPermanente;
+import modeloDatos.ChoferTemporario;
 import modeloDatos.Cliente;
 import modeloDatos.Pedido;
 import modeloDatos.Vehiculo;
+import modeloDatos.Viaje;
 import modeloNegocio.Empresa;
 import util.Constantes;
 
@@ -62,7 +64,7 @@ public class TestEmpresa {
 		assertTrue("La lista debería contener el chofer agregado", Choferes.containsKey(chofer.getDni()));
 				
 		
-		Empresa empresa3 = null;
+		/*Empresa empresa3 = null;
 		
 		try {
 			empresa3.agregarChofer(chofer);
@@ -70,7 +72,7 @@ public class TestEmpresa {
 		}
 		
 		assertNull("Se añadió el chofer correctamente (no debería pasar)", Choferes.containsKey(chofer.getDni()));
-		
+		*/
 		try {
 			empresa.agregarChofer(chofer);
 			fail("Se esperaba la excepcion ChoferRepetidoException.");
@@ -93,7 +95,7 @@ public class TestEmpresa {
 		
 		assertTrue("La lista debería contener el cliente agregado", Clientes.containsKey("agus"));
 		
-		Empresa empresa3 = null;
+		/*Empresa empresa3 = null;
 		
 		try {
 			empresa3.agregarCliente("agus", "1234", "Agustin");
@@ -103,7 +105,7 @@ public class TestEmpresa {
 		HashMap<String, Cliente> Clientes3 = empresa3.getClientes();
 		
 		assertNull("Se añadió el cliente correctamente (no debería pasar)", Clientes3.containsKey("agus"));
-		
+		*/
 		try {
 			empresa.agregarCliente("agus", "1234", "Agustin");
 			fail("Se esperaba la exception UsuarioYaExisteException.");
@@ -124,7 +126,7 @@ public class TestEmpresa {
 		
 		assertTrue("La lista debería contener el vehiculo agregado", Vehiculos.containsKey("ABC123"));
 		
-		Empresa empresa2 = null;
+		/*Empresa empresa2 = null;
 		
 		Vehiculo v2 = new Auto("DEF456",2,false);
 		HashMap<String, Vehiculo> Vehiculos2 = empresa.getVehiculos();
@@ -135,7 +137,7 @@ public class TestEmpresa {
 		}
 		
 		assertNull("Se añadió el vehiculo correctamente (no debería pasar)", Vehiculos2.containsKey("DEF123"));
-		
+		*/
 		
 		try {
 			empresa.agregarVehiculo(v);
@@ -152,9 +154,8 @@ public class TestEmpresa {
 	@Test
 	public void testAgregarPedido()
 	{
-		Vehiculo v = new Auto("ABC123",4,true); // Testeado previamente.
+		Vehiculo v = new Auto("ABC123",4,false); // Testeado previamente.
 		Vehiculo v2 = new Auto("DEF456",2,false);
-		Vehiculo v3 = new Auto("GHI789",4,false);
 		
 		try {
 			empresa.agregarVehiculo(v);
@@ -171,7 +172,9 @@ public class TestEmpresa {
 		} catch (UsuarioYaExisteException e) {
 		}
 		
-		Pedido pedido = new Pedido(cliente, 3,false,true,10,Constantes.ZONA_PELIGROSA);
+		Cliente clienteGet = empresa.getClientes().get("agus");
+		
+		Pedido pedido = new Pedido(clienteGet, 3,false,true,10,Constantes.ZONA_PELIGROSA);
 		
 		try {
 			empresa.agregarPedido(pedido);
@@ -181,23 +184,29 @@ public class TestEmpresa {
 		
         HashMap<Cliente, Pedido> Pedidos = empresa.getPedidos();
 		
-		assertTrue("La lista debería contener el pedido agregado", Pedidos.containsKey(cliente));
+        
+		assertTrue("La lista debería contener el pedido agregado", Pedidos.containsKey(clienteGet));
 		
 		// CASO 2
 		
 		Cliente cliente2 = new Cliente("Pedrito","1234","pedro");
 		
+		try {
+			empresa.agregarCliente("Pedrito","1234","pedro");
+		} catch (UsuarioYaExisteException e) {
+		}
+		
+		
         Pedido pedido2 = new Pedido(cliente2, 3,false,true,5,Constantes.ZONA_SIN_ASFALTAR);
 		
 		try {
 			empresa.agregarPedido(pedido2);
-			fail("Se esperaba la expección ClienteNoExisteException.");
+			fail("Se esperaba la excepción ClienteNoExisteException.");
 		} catch (SinVehiculoParaPedidoException | ClienteNoExisteException | ClienteConViajePendienteException
 				| ClienteConPedidoPendienteException e) {
-			assertTrue("La excepción capturada no es ChoferRepetidoException", e instanceof ClienteNoExisteException);
+			assertTrue("La excepción capturada no es ClienteNoExisteException", e instanceof ClienteNoExisteException);
+
 		}
-		
-		
 		
 		// CASO 3
 		
@@ -208,7 +217,9 @@ public class TestEmpresa {
 		} catch (UsuarioYaExisteException e) {
 		}
 		
-		Pedido pedido3 = new Pedido(cliente3, 4,true,true,6,Constantes.ZONA_STANDARD);
+		Cliente clienteGet3 = empresa.getClientes().get("Manu");
+		
+		Pedido pedido3 = new Pedido(clienteGet3, 4,true,true,6,Constantes.ZONA_STANDARD);
 		try {
 			empresa.agregarPedido(pedido3);
 			fail("Se esperaba la excepción SinVehiculoParaPedido.");
@@ -219,7 +230,7 @@ public class TestEmpresa {
 		
 		
 		// CASO 4
-		Empresa empresa2 = null;
+		/*Empresa empresa2 = null;
 		
 		try {
 			empresa2.agregarVehiculo(v);
@@ -234,9 +245,96 @@ public class TestEmpresa {
 		
 		HashMap<Cliente, Pedido> Pedidos2 = empresa2.getPedidos();
 		
-		assertNull("Se agrego el pedido correctamente (no debería pasar)", Pedidos2.containsKey(cliente));
+		assertNull("Se agrego el pedido correctamente (no debería pasar)", Pedidos2.containsKey(cliente)); */
+		
+		
+		// CASO 5 - AGREGO EL PEDIDO DEL CASO 1 DE NUEVO
+		
+		try {
+			empresa.agregarPedido(pedido);
+			fail("Se esperaba la excepción ClienteConPedidoPendiente.");
+		} catch (SinVehiculoParaPedidoException | ClienteNoExisteException | ClienteConViajePendienteException
+				| ClienteConPedidoPendienteException e) {
+			assertTrue("La excepción capturada no es ClienteConPedidoPendienteException", e instanceof ClienteConPedidoPendienteException);
+		}
+		
+		// CASO 6 - AGREGO EL PEDIDO DEL CASO 1 DE NUEVO
+		// PRIMERO VINCULO UN CHOFER AL PEDIDO QUE ES EL UNICO QUE ESTA EN LA LISTA
+		
+		Chofer chofer = new ChoferPermanente("12345","Jorge",2000,0); // Se que funciona por el test de la capa de datos
+		
+		
+		// FALTA CREAR VIAJE, UNA VEZ TESTEADO LO USO
+		
+		try {
+			empresa.agregarChofer(chofer);
+		} catch (ChoferRepetidoException e) {
+		}
+		
+		try {
+			empresa.agregarPedido(pedido);
+			fail("Se esperaba la excepción ClienteConViajePendiente.");
+		} catch (SinVehiculoParaPedidoException | ClienteNoExisteException | ClienteConViajePendienteException
+						| ClienteConPedidoPendienteException e) {
+				assertTrue("La excepción capturada no es ClienteConViajePendienteException", e instanceof ClienteConViajePendienteException);
+		}
 		
 		
 	}
-}
+
 	
+	@Test
+	public void testCrearViaje(){
+		// ESCENARIO INICIAL
+		
+		Vehiculo v = new Auto("HUD557",4,false); // Testeado previamente.
+		Vehiculo v2 = new Auto("ASD123",4,true);
+		
+		try {
+			empresa.agregarVehiculo(v);
+			empresa.agregarVehiculo(v2);
+		} catch (VehiculoRepetidoException e) {
+		}
+		
+		Chofer chofer = new ChoferPermanente("40159003","Anacleto",2000,0); // Se que funciona por el test de la capa de datos
+		
+		try {
+			empresa.agregarChofer(chofer);
+		} catch (ChoferRepetidoException e) {
+		}
+
+		Cliente cliente1 = new Cliente("agus","1234","agustin");
+		Cliente cliente2 = new Cliente("manu","4567","manuel");
+		Cliente cliente3 = new Cliente("lauti","5678","lautaro");
+		Cliente cliente4 = new Cliente("luken","1234","lucas");
+		
+		try {
+			empresa.agregarCliente("agus", "1234", "Agustin");
+			empresa.agregarCliente("manu","4567","manuel");
+			empresa.agregarCliente("lauti","5678","lautaro");
+			empresa.agregarCliente("luken","1234","lucas");
+		} catch (UsuarioYaExisteException e) {
+		}
+		
+		Cliente clienteGet1 = empresa.getClientes().get("Agustin");
+		Cliente clienteGet2 = empresa.getClientes().get("manuel");
+		Cliente clienteGet3 = empresa.getClientes().get("lautaro");
+		Cliente clienteGet4 = empresa.getClientes().get("lucas");
+		
+		Pedido pedido1 = new Pedido(clienteGet1, 2,false,false,3,Constantes.ZONA_PELIGROSA);
+		Pedido pedido2 = new Pedido(clienteGet2, 2,false,false,3,Constantes.ZONA_PELIGROSA);
+		Pedido pedido3 = new Pedido(clienteGet3, 5,false,true,3,Constantes.ZONA_PELIGROSA);
+		Pedido pedido4 = new Pedido(clienteGet1, 3,false,false,3,Constantes.ZONA_SIN_ASFALTAR);
+		Pedido pedido5 = new Pedido(clienteGet4, 4,true,false,3,Constantes.ZONA_SIN_ASFALTAR);
+		Pedido pedido6 = new Pedido(clienteGet1, 3,false,false,3,Constantes.ZONA_PELIGROSA);
+		
+		
+		//Viaje viaje = new Viaje(pedido, chofer, auto);
+	
+	}
+}
+
+
+
+
+
